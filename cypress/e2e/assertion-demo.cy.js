fc1f4a44ecbf266477ccfb4975047ec033d1955b
Vaibhav Matere <vpm00718897@gmail.com>
2023-07-14 15:50:44 +0530

    should

--- cypress/e2e/assertion-demo.cy.js
+++ cypress/e2e/assertion-demo.cy.js
@@ -3,6 +3,12 @@ describe("check assertions", () => {
           cy.visit("https://example.cypress.io")
           cy.contains('get').click();
 
-
+          cy.get('#query-btn')
+               .should("contain", "Button")
+               .should("have.class", "query-btn")
+          //   .should("be.disabled")
+          // this test will fail as the btn is enabled in UI.
+               .should("be.enabled")
+               .should("be.visible")
      })
 });
